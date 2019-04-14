16331854a59c9d182742e87de9f6b3f6fe2938a4
Toms <tomas.manuel.vazquez@gmail.com>
2019-04-14 17:06:27 -0300

    v 0.7.2 add new pet to databse

--- app/src/main/java/com/applications/toms/juegodemascotas/view/MyPetsActivity.java
+++ app/src/main/java/com/applications/toms/juegodemascotas/view/MyPetsActivity.java
@@ -1,7 +1,10 @@
 package com.applications.toms.juegodemascotas.view;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
@@ -12,9 +15,19 @@ import android.widget.Toast;
 
 import com.applications.toms.juegodemascotas.R;
 import com.applications.toms.juegodemascotas.controller.PetsFromOwnerController;
+import com.applications.toms.juegodemascotas.model.Duenio;
 import com.applications.toms.juegodemascotas.model.Mascota;
 import com.applications.toms.juegodemascotas.util.ResultListener;
 import com.applications.toms.juegodemascotas.view.adapter.MyPetsAdapter;
+import com.applications.toms.juegodemascotas.view.fragment.AddPetFragment;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -22,6 +35,11 @@ import java.util.List;
 public class MyPetsActivity extends AppCompatActivity implements MyPetsAdapter.AdapterInterface {
 
     public static final String KEY_DUENIO_ID = "duenio";
+
+    private FirebaseDatabase mDatabase;
+    private static DatabaseReference mReference;
+    private static FirebaseUser currentUser;
+
 
     //Atributos
     private MyPetsAdapter myPetsAdapter;
@@ -31,6 +49,11 @@ public class MyPetsActivity extends AppCompatActivity implements MyPetsAdapter.A
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_pets);
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+        currentUser = mAuth.getCurrentUser();
+        mDatabase = FirebaseDatabase.getInstance();
+        mReference = mDatabase.getReference();
 
         FloatingActionButton fabAddPet = findViewById(R.id.fabAddPet);
 
@@ -60,8 +83,11 @@ public class MyPetsActivity extends AppCompatActivity implements MyPetsAdapter.A
         fabAddPet.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO agregar mascota
-                Toast.makeText(MyPetsActivity.this, "En construccion", Toast.LENGTH_SHORT).show();
+                AddPetFragment addPetFragment = new AddPetFragment();
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.containerPets,addPetFragment);
+                fragmentTransaction.commit();
             }
         });
 
@@ -70,5 +96,34 @@ public class MyPetsActivity extends AppCompatActivity implements MyPetsAdapter.A
     @Override
     public void goToProfile(String idOwner) {
         //TODO ir al profile
+    }
+
+    public static void addPetToDataBase(final String name, final String raza, final String size, final String birth, final String sex, final String photo, final String info){
+        final List<Mascota> mascotaList = new ArrayList<>();
+        mReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot childSnapShot : dataSnapshot.getChildren()){
+                    Duenio duenio = childSnapShot.getValue(Duenio.class);
+                    if (duenio.getUserId().equals(currentUser.getUid())){
+                        if (duenio.getMisMascotas()!=null){
+                            mascotaList.addAll(duenio.getMisMascotas());
+                        }
+
+                        String idPet = childSnapShot.getKey();
+                        Mascota newMascota = new Mascota(idPet,name,raza,size,sex,birth,photo,info,idPet);
+                        mascotaList.add(newMascota);
+                        mReference.child(idPet).child("misMascotas").setValue(mascotaList);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
